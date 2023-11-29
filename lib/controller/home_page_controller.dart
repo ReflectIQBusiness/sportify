4c33254be35510d7a30c4312e5325693a5f4dac5
reflectiqbusiness <reflectiqbusiness@gmail.com>
2023-11-29 15:59:26 +0100

    last version

--- lib/controller/home_page_controller.dart
+++ lib/controller/home_page_controller.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'dart:ffi';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
